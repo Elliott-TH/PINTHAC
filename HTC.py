256de8fd3a7743f12a3d99e13e5cd4f94376aefb
Elliott <elliotthirko1@gmail.com>
2026-08-28 21:18:25 -0400

    Adding HTC file

---
+++ HTC.py
@@ -0,0 +1,40 @@
+import numpy as np
+import torch
+import scipy
+
+
+def Dittus():
+    return
+
+def Petchukov():
+    return
+
+def Gnielinski():
+    return
+
+def SchrockGrossman():
+    return
+
+def Chen_H2O_dT():
+    return
+
+def Chen_H2O():
+    return
+
+def Bjorge_dT():
+    return
+
+def Bjorge():
+    return
+
+def Swenson_dT():
+    return
+
+def Swenson():
+    return 
+
+def Chen_SCW_dT():
+    return
+
+def Chen_SCW():
+    return
