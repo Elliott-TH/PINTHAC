ae2a7a46c803d2c7426b826da2c0f3435c62f948
Elliott <elliotthirko1@gmail.com>
2026-09-01 19:56:50 -0400

    Adding (empty) friction module

---
+++ FRICT.py
@@ -0,0 +1,19 @@
+import numpy
+import torch
+import scipy
+import array_api_compat as aac
+
+def lib(x):
+    return import scipy
+import array_api_compat.array_namespace(x)
+
+
+class f:
+    def blah():
+        return
+
+
+class Spacer:
+    def blah2():
+        return
+
